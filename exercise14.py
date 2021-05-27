3f9ffb7fbb991f2807e7c87dace8a906b4cbaee3
Rey Arnoco <60849862+RArnoco@users.noreply.github.com>
2021-05-28 00:02:40 +0800

    exercises

---
+++ exercise14.py
@@ -0,0 +1,29 @@
+#exercise14_Prompting and Passing
+#argv
+from sys import argv
+
+script, user_name = argv
+prompt = '> '
+
+print (f"Hi {user_name}, I'm the {script} script.")
+print ("I'd like to ask you some few questions.")
+print (f"Do you like me {user_name}?")
+likes = input (prompt)
+
+#prompts for lives value
+print (f"Where do you live {user_name}?")
+lives = input (prompt)
+
+#prompts for computwer value
+print (f"What kind of computer did you have?")
+computer = input (prompt)
+
+#combine input prompts
+print (f"""
+Alright, so you said {likes} about liking me.
+You live in {lives}. Not sure where that is.
+And you have a {computer} computer. Nice.  
+""")
+
+
+
